1c6858caf96019555fe764e1b8ac1589ace1313f
Josh <grafton.josh@gmail.com>
2018-09-17 09:22:26 -0600

    started git repo

---
+++ GoogleAPI.js
@@ -0,0 +1,3 @@
+
+// //Below URL is a Text Searcg that returns address, name, open_now, photos, placeID, pricelevel, rating, and type such as restaurant, bar, cafe
+// https://maps.googleapis.com/maps/api/place/textsearch/xml?query=dog+friendly+restaurants+boulder&key=AIzaSyC9G5N9yXiqKofp4G21tb-D_QN8bAvgXDI
