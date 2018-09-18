9fb47c90697f42b988fd238fa3324cbe8cfa96ee
Josh <grafton.josh@gmail.com>
2018-09-18 10:30:40 -0600

    Added location autocomplete

--- GoogleAPI.js
+++ GoogleAPI.js
@@ -5,8 +5,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
   //Step 0: Ensure event listener works
 
   //Step 1: fetch some data from a server.
-  let location = 'lakeland'
-  let url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=dog+friendly+restaurants+${location}&key=AIzaSyC9G5N9yXiqKofp4G21tb-D_QN8bAvgXDI`
+  let location = 'Boulder, CO, USA'
+  //let url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=dog+friendly+restaurants+${location}&key=AIzaSyC9G5N9yXiqKofp4G21tb-D_QN8bAvgXDI`
   axios.get(url)
     .then((response) => {
   //Step 2: Log Data and see what you are getting back as a response
