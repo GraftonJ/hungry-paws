f853f6feabe217c027685c0840125da12eb1c677
Josh <grafton.josh@gmail.com>
2018-09-20 11:51:24 -0600

    Linked css

--- GoogleAPI.js
+++ GoogleAPI.js
@@ -1,5 +1,3 @@
-// //Below URL is a Text Searcg that returns address, name, open_now, photos, placeID, pricelevel, rating, and type such as restaurant, bar, cafe
-// https://maps.googleapis.com/maps/api/place/textsearch/json?query=dog+friendly+restaurants+boulder&key=AIzaSyC9G5N9yXiqKofp4G21tb-D_QN8bAvgXDI
 const apiKey = 'AIzaSyC9G5N9yXiqKofp4G21tb-D_QN8bAvgXDI'
 document.addEventListener('DOMContentLoaded', (event) => {
   //Find the price level of the restaurant
