71e408d434fda495549b6724bcd19212346de479
Josh <grafton.josh@gmail.com>
2018-09-20 12:49:00 -0600

    Setup testing

--- test/tests.js
+++ test/tests.js
@@ -7,25 +7,25 @@ describe('check tests are running', () => {
 })
 
 //Test to see if it is a function
-describe('test DOM helpers', () => {
-  it('createCards exist', () => {
-    expect(createCards).to.be.a("function")
+describe('test priceLevel', () => {
+  it('priceLevel exist', () => {
+    expect(priceLevel).to.be.a("function")
   })
-  it('does it return a div dom element', () => {
-    let div = createCards("param1", "param2" )
-    expect(div.tagName).to.equal("DIV")
-  })
-  it('does it create materialize card element', () => {
-    let div = createCards("param1", "param2" )
-    expect(div.children.length).to.equal(2)
-    expect(div.children[0]).to.equal('IMG')
-    expect(div.children[1]).to.equal('DIV')
-    expect(div.className).to.equal('media')
-
-    let h5 = div.children[1].children[0]
-    expect(h5.tagname).to.equal('H5')
-    expect(h5.tagname).to.equal('H5')
-    expect(h5.innerText).to.equal('title')
-
-  })
-})
+//   it('does it return a div dom element', () => {
+//     let div = createCards("param1", "param2" )
+//     expect(div.tagName).to.equal("DIV")
+//   })
+//   it('does it create materialize card element', () => {
+//     let div = createCards("param1", "param2" )
+//     expect(div.children.length).to.equal(2)
+//     expect(div.children[0]).to.equal('IMG')
+//     expect(div.children[1]).to.equal('DIV')
+//     expect(div.className).to.equal('media')
+//
+//     let h5 = div.children[1].children[0]
+//     expect(h5.tagname).to.equal('H5')
+//     expect(h5.tagname).to.equal('H5')
+//     expect(h5.innerText).to.equal('title')
+//
+//   })
+ })
