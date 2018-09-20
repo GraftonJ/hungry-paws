8fe17e5743213423228f34e7df9fc49d045cd739
Josh <grafton.josh@gmail.com>
2018-09-20 15:50:55 -0600

    Working site with callouts

--- GoogleAPI.js
+++ GoogleAPI.js
@@ -1,5 +1,32 @@
 const apiKey = 'AIzaSyC9G5N9yXiqKofp4G21tb-D_QN8bAvgXDI'
 
+//Function to create materialze cards based on API data and search
+function createResultCard(result) {
+  //Determine if business is open or closed at the moment
+  let checkOpen = openClosed(result.opening_hours.open_now)
+  //Determine Price of the restuarant
+  let price = priceLevel(result.price_level)
+  //Set reference to pull in a photo of the business
+  let photoReference = result.photos[0]['photo_reference']
+  document.getElementById('cardArea').innerHTML += `
+  <div class="col s12 m4 l4">
+    <div class="card medium">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator responsive-img" src="https://maps.googleapis.com/maps/api/place/photo?maxheight=1000&maxwidth=1000&photoreference=${photoReference}&key=${apiKey}">
+      </div>
+      <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">${result.name}<i class="material-icons right">more_vert</i></span>
+        <p>Price: ${price}<br>Rating: ${result.rating}<br>${checkOpen}</p>
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">More Info<i class="material-icons right">close</i></span>
+        <p>${checkOpen} <br> Rating : ${result.rating} <br> Price: ${price} <br> Address: <a href="https://www.google.com/maps/search/?api=1&query=${result.name}+${result.formatted_address}" target="_blank">${result.formatted_address}</a></p>
+      </div>
+    </div>
+  </div>`
+
+}
+
 //Find the price level of the restaurant
 
 function priceLevel(price) {
@@ -27,11 +54,11 @@ function priceLevel(price) {
 
 //Determine if business is open or closed at time of Search
 function openClosed(open) {
-  if (open) {
-    return 'Open Now'
-  } else {
-    return 'Currently Closed'
-  }
+    if (open) {
+      return 'Open Now'
+    } else {
+      return 'Currently Closed'
+    }
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
@@ -48,7 +75,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     //Set local storage of search location
     localStorage.setItem('location', document.getElementById('location').value)
     let location = document.getElementById('location').value
-    console.log(location);
     //Set local storage of place type
     let placeType = document.getElementById('placeType').value
       url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=dog+friendly+${placeType}+${location}&key=${apiKey}`
@@ -71,32 +97,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
         //Loop through results and find the names of the restaurants, pictures, and address and create a materialize card for each one
         response.data.results.forEach(createResultCard)
-
-        function createResultCard(result) {
-          //Determine if business is open or closed at the moment
-          let checkOpen = openClosed(result.opening_hours.open_now)
-          //Determine Price of the restuarant
-          let price = priceLevel(result.price_level)
-          //Set reference to pull in a photo of the business
-          let photoReference = result.photos[0]['photo_reference']
-
-          document.getElementById('cardArea').innerHTML += `
-          <div class="col s12 m4 l4">
-            <div class="card medium">
-              <div class="card-image waves-effect waves-block waves-light">
-                <img class="activator responsive-img" src="https://maps.googleapis.com/maps/api/place/photo?maxheight=1000&maxwidth=1000&photoreference=${photoReference}&key=${apiKey}">
-              </div>
-              <div class="card-content">
-                <span class="card-title activator grey-text text-darken-4">${result.name}<i class="material-icons right">more_vert</i></span>
-                <p>Price: ${price}<br>Rating: ${result.rating}<br>${checkOpen}</p>
-              </div>
-              <div class="card-reveal">
-                <span class="card-title grey-text text-darken-4">More Info<i class="material-icons right">close</i></span>
-                <p>${checkOpen} <br> Rating : ${result.rating} <br> Price: ${price} <br> Address: <a href="https://www.google.com/maps/search/?api=1&query=${result.name}+${result.formatted_address}" target="_blank">${result.formatted_address}</a></p>
-              </div>
-            </div>
-          </div>`
-        }
+        //Call the create result card function to make a materialze card for each place in the api
+        createResultCard()
       })
   })
 })
